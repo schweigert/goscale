5bdd3c8f68d96a4d96bfda36f0519d4051df0ddf
Marlon Henry Schweigert <fleyhe0@gmail.com>
2018-10-17 14:07:20 -0300

    Bootstrap

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # goscale
 Proxy over multiples TCP connections
+
+# Usage
+
+```
+  go get github.com/schweigert/goscale
+  go install github.com/schweigert/goscale
+
+  PULLSIZE=3 EP_0=localhost:3030 EP_1=localhost:3031 EP_2=localhost:3032 BIND=0.0.0.0:3000 goscale
+```
---
+++ goscale.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"sync"
+
+	"github.com/schweigert/lampper"
+)
+
+func main() {
+	bind := os.Getenv("BIND")
+	pullSize := os.Getenv("PULLSIZE")
+
+	size, err := strconv.Atoi(pullSize)
+	if err != nil {
+		panic(err)
+	}
+
+	service, err := lampper.Listen("tcp", bind)
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		peer := service.Accept()
+		go handle(randomAddr(rand.Intn(size)), peer)
+	}
+}
+
+func randomAddr(el int) string {
+	elStr := strconv.Itoa(el)
+	return os.Getenv("EP_" + elStr)
+}
+
+func handle(addr string, peer1 *lampper.Peer) {
+	log.Println("Dial to:", addr)
+	peer2, err := lampper.Dial("tcp", addr)
+	if err != nil {
+		log.Println("Error to dial:", addr)
+		return
+	}
+
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+
+	go pipe(wg, peer1, peer2)
+	go pipe(wg, peer2, peer1)
+
+	wg.Wait()
+	log.Println("Handle closed to:", addr)
+}
+
+func pipe(wg *sync.WaitGroup, peer1, peer2 *lampper.Peer) {
+	defer wg.Done()
+
+	for {
+		bytes, err := peer1.ReadBytes(1)
+		if err != nil {
+			return
+		}
+
+		err = peer2.WriteBytes(bytes)
+		if err != nil {
+			return
+		}
+	}
+}
